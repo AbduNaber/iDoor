af593db7ba35b2af8863f0c9682fb22e75b286e6
Abdu <abdturkmen@hotmail.com>
2023-01-30 16:15:11 +0300

    some changes were made

--- website/project.py
+++ website/project.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     req = requests.get(
-        'https://api.thingspeak.com/channels/1995263/feeds.json?api_key=KPAHRPU02P3AIXNW&results=2 ')
+        'https://api.thingspeak.com/channels/1995263/feeds.json?api_key=${YOUR_API_KEY}&results=2 ')
     state1 = req.json()["feeds"][-1]["field1"]
     return render_template("website.html", state=state1)
 
@@ -15,7 +15,7 @@ def home():
 @app.get("/update")
 def update():
     req = requests.get(
-        'https://api.thingspeak.com/channels/1995263/feeds.json?api_key=KPAHRPU02P3AIXNW&results=2 ')
+        'https://api.thingspeak.com/channels/1995263/feeds.json?api_key=${YOUR_API_KEY}&results=2 ')
     state1 = req.json()["feeds"][-1]["field1"]
     time1 = req.json()["feeds"][-1]["created_at"].split("T")
     hour = time1[1].split(".")[0].split("Z")[0]
